f433201a10beb6f0e66e7241be07a200e6df2391
Arnold Ayaoan Jr <arnoldayaoanjr@yahoo.com>
2020-04-22 13:25:15 +0800

    Added missing files

---
+++ src/main/java/com/outgrowthsolutions/ogsrecipeapp/OgsRecipeAppMongoApplication.java
@@ -0,0 +1,13 @@
+package com.outgrowthsolutions.ogsrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgsRecipeAppMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgsRecipeAppMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/outgrowthsolutions/ogsrecipeapp/OgsRecipeAppMongoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.outgrowthsolutions.ogsrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgsRecipeAppMongoApplicationTest {
+@Test
+    void contextLoads(){
+
+}
+}
